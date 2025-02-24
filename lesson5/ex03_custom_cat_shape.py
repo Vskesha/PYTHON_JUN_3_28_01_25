e5fb9623b58e7e53ebb171ee84673de4d803f3d4
vskesha <vskesha@gmail.com>
2025-02-25 01:09:05 +0200

    changes in custom turtle shape

--- lesson5/ex03_custom_cat_shape.py
+++ lesson5/ex03_custom_cat_shape.py
@@ -1,10 +1,15 @@
 import turtle
+from pathlib import Path
 
 screen = turtle.Screen()
 t = turtle.Turtle()
 
-screen.addshape("cat_cry.gif")
-t.shape("cat_cry.gif")
+image_name = "cat_cry.gif"
+# print(Path(__file__).parent.joinpath(image_name))
+# image_path = str(Path(__file__).parent.joinpath(image_name)) 
+
+screen.addshape(image_name)
+t.shape(image_name)
 t.forward(100)
 
 turtle.done()
