90b0da72b539623e9d76dd17e495754983a71596
vskesha <vskesha@gmail.com>
2025-02-25 13:51:12 +0200

    change path to image in custom form

--- lesson5/ex03_custom_cat_shape.py
+++ lesson5/ex03_custom_cat_shape.py
@@ -5,11 +5,13 @@ screen = turtle.Screen()
 t = turtle.Turtle()
 
 image_name = "cat_cry.gif"
-# print(Path(__file__).parent.joinpath(image_name))
-# image_path = str(Path(__file__).parent.joinpath(image_name)) 
+print(Path(__file__).parent.joinpath(image_name))
+image_path = str(Path(__file__).parent.joinpath(image_name)) 
 
-screen.addshape(image_name)
-t.shape(image_name)
+screen.addshape(image_path)
+t.shape(image_path)
+t.forward(100)
+t.left(90)
 t.forward(100)
 
 turtle.done()
