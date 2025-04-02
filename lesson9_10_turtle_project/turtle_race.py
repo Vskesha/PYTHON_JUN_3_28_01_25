b3906af3470780e65e86a8d4eca44f5260cec8fa
vskesha <vskesha@gmail.com>
2025-04-02 18:55:17 +0300

    come back

--- lesson9_10_turtle_project/turtle_race.py
+++ lesson9_10_turtle_project/turtle_race.py
@@ -10,14 +10,9 @@ fheight = 600
 border = 20
 max_move = 10
 number_of_obstacles = 35
-number_of_boosts = 30
-player_step = 15
-player_position = -1
-boosts_color = "lightgreen"
 colors = ["red", "blue", "green", "gray", "yellow", "orange", "purple", "brown"]
 turtles = []
 obstacles = []
-boosts = []
 #--------------------------------------------------------------------------------
 hwidth = fwidth // 2
 hheight = fheight // 2
@@ -30,8 +25,6 @@ screen = turtle.Screen()
 screen.title("Черепаші перегони")
 screen.bgcolor("lightblue")
 screen.setup(width=fwidth + border * 2, height=fheight + border * 2)
-
-player = turtle.Turtle()
 
 status_pen = turtle.Turtle()
 status_pen.penup()
@@ -46,23 +39,9 @@ def start_game(x, y):
 
     generate_obstacles()
 
-    generate_boosts()
-
     num_players = get_number_of_players()
 
     generate_turtles(num_players)
-
-    status_pen.write(
-        "Використовуй стрілки для переміщення черепахи",
-        align="center",
-        font=("Arial", 16, "bold")
-    )
-
-    choose_turtle(num_players)
-
-    set_speed()
-
-    countdown()
 
     start_race()
     
@@ -144,47 +123,23 @@ def generate_turtles(num_players):
         turtles.append(bot)
     
 def start_race():
-
-    global player
-    player = turtles[player_position]
-
-    turtle.listen()
-    turtle.onkey(move_left, "Left")
-    turtle.onkey(move_right, "Right")
-    turtle.onkey(move_up, "Up")
-    turtle.onkey(move_down, "Down")
-
     turtle.tracer(0)
     screen.onscreenclick(None)
     game_in_progress = True
     while game_in_progress:
         for bot in turtles:
+            direction = 90
+            for obs in obstacles:
+                if bot.distance(obs) < 20:
+                    if bot.xcor() < obs.xcor():
+                        direction = 145
+                    else:
+                        direction = 45
+                    break
+            bot.setheading(direction)
 
-            for boost in boosts:
-                if bot.distance(boost) < 20:
-                    bot.setheading(90)
-                    bot.pensize(5)
-                    bot.forward(random.randint(40, 60))
-                    bot.pensize(1)
-
-            if bot != player:
-                direction = 90
-                for obs in obstacles:
-                    if bot.distance(obs) < 20:
-                        if bot.xcor() < obs.xcor():
-                            direction = 145
-                        else:
-                            direction = 45
-                        break
-                bot.setheading(direction)
-
-                bot.forward(random.randint(1, max_move))
-
+            bot.forward(random.randint(1, max_move))
             if bot.ycor() >= finish:
-                turtle.onkey(None, "Left")
-                turtle.onkey(None, "Right")
-                turtle.onkey(None, "Up")
-                turtle.onkey(None, "Down")
                 game_in_progress = False
                 declare_winner(bot)
                 break
@@ -193,11 +148,6 @@ def start_race():
         time.sleep(delay)
 
     status_pen.clear()
-    turtle.update()
-
-    time.sleep(2)
-    draw_start_button()
-    screen.onscreenclick(start_game)
 
 def declare_winner(winner):
     winner.penup()
@@ -240,99 +190,6 @@ def generate_obstacles():
         obs.goto(x, y)
         obstacles.append(obs)
 
-def generate_boosts():
-    boosts.clear()
-    for _ in range(number_of_boosts):
-        x = random.randint(-hwidth + border, hwidth - border)
-        y = random.randint(start + border, finish - border)
-        boost = turtle.Turtle()
-        boost.speed(0)
-        boost.shape("square")
-        boost.color(boosts_color)
-        boost.penup()
-        boost.goto(x, y)
-        boosts.append(boost)
-
-def can_move_to(x, y):
-    if x < -hwidth or x > hwidth or y < start:
-        return False
-    
-    for obs in obstacles:
-        if obs.distance(x, y) < player_step:
-            return False
-    
-    return True
-
-def move_left():
-    x = player.xcor()
-    y = player.ycor()
-    x -= player_step
-    if can_move_to(x, y):
-        player.setheading(180)
-        player.goto(x, y)
-
-def move_right():
-    x = player.xcor()
-    y = player.ycor()
-    x += player_step
-    if can_move_to(x, y):
-        player.setheading(0)
-        player.goto(x, y)
-
-def move_up():
-    x = player.xcor()
-    y = player.ycor()
-    y += player_step
-    if can_move_to(x, y):
-        player.setheading(90)
-        player.goto(x, y)
-
-def move_down():
-    x = player.xcor()
-    y = player.ycor()
-    y -= player_step
-    if can_move_to(x, y):
-        player.setheading(270)
-        player.goto(x, y)
-
-def countdown():
-    pen = turtle.Turtle()
-    pen.penup()
-    pen.hideturtle()
-    pen.goto(0, -hheight // 2)
-
-    for i in range(3, 0, -1):
-        pen.color(colors[i])
-        pen.write(i, align="center", font=("Arial", hheight, "bold"))
-        time.sleep(1.5)
-        pen.clear()
-
-    pen.color(colors[0])
-    pen.write("Start", align="center", font=("Arial", hheight // 2, "bold"))
-    time.sleep(1)
-    pen.clear()
-
-def set_speed():
-    global delay
-    user_speed = screen.numinput(
-        "Встановлення швидкості",
-        "Вибери швидкість (1 - 20)",
-        minval=1,
-        maxval=20,
-    )
-    delay = 1 / (5 + user_speed)
-
-def choose_turtle(num_players):
-    global player_position
-
-    position = screen.numinput(
-        "Вибір черепахи",
-        f"Введи номер черепахи (1 - {num_players}):",
-        minval=1,
-        maxval=num_players,
-    )
-
-    player_position = int(position) - 1
 
 # Відслідковування натискання на кнопку
 draw_start_button()
@@ -341,12 +198,12 @@ screen.onscreenclick(start_game)
 
 turtle.done()
 
-# команди для створення EXE-файлу
+# команди для перетворення в запускний файл
 # 
 # pip install pyinstaller
 
 # Windows 
-# pyinstaller --onefile --windowed --icon=turtle1.ico turtle2.py
+# pyinstaller --onefile --windowed --icon=turtle1.ico turtle_race.py
 
 # Mac
-# pyinstaller --onefile --windowed --icon=turtle1.icns turtle2.py
+# pyinstaller --onefile --windowed --icon=turtle1.icns turtle_race.py
