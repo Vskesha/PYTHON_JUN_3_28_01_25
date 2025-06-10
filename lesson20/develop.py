db31275815018b882d4305627c64c74e3cedbb99
Vasyl Boliukh <vskesha@gmail.com>
2025-06-10 17:25:37 +0300

    added shooting

--- lesson20/develop.py
+++ lesson20/develop.py
@@ -11,6 +11,7 @@ idle_image = pygame.image.load(folder_path.joinpath("idle.png"))
 jump_image = pygame.image.load(folder_path.joinpath("jump.png"))
 crouch_image = pygame.image.load(folder_path.joinpath("crouch.png"))
 attack_image = pygame.image.load(folder_path.joinpath("attack.png"))
+shooting_image = pygame.image.load(folder_path.joinpath("shoot.png"))
 
 x = 300
 y = y_start = 500
@@ -18,6 +19,8 @@ vertical_speed = 40
 is_jumping = False
 is_crouching = False
 is_attacking = False
+shot_timer = 0
+return_to_idle = False
 y_speed = 0
 gravity = 4
 image = idle_image
@@ -41,13 +44,19 @@ while True:
             elif event.key == pygame.K_a and not (is_jumping or is_crouching):
                 is_attacking = True
                 image = attack_image
+            elif event.key == pygame.K_s:
+                shot_timer = 5
+                if image == idle_image:
+                    image = attack_image
+                    if not is_attacking:
+                        return_to_idle = True
         elif event.type == pygame.KEYUP:
             if event.key == pygame.K_DOWN:
                 is_crouching = False
                 image = idle_image
             elif event.key == pygame.K_a:
                 is_attacking = False
-                image = idle_image
+                image = idle_image           
     
     if is_jumping:
         y -= y_speed
@@ -57,7 +66,19 @@ while True:
             is_jumping = False
             image = idle_image
 
-    screen.fill((255, 255, 255))
+    screen.fill((255, 255, 255))    
     screen.blit(image, (x, y - image.get_height()))
+    
+    if shot_timer:
+        screen.blit(
+            shooting_image,
+            (x + image.get_width(),
+             y - image.get_height() + 30)
+        )
+        shot_timer -= 1
+    elif return_to_idle:
+        image = idle_image
+        return_to_idle = False 
+
     pygame.display.flip()
     clock.tick(fps)
