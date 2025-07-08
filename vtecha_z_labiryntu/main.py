9cc8d7a4546a3f4a0470084a0df5515284b24e2c
Vasyl Boliukh <vskesha@gmail.com>
2025-07-08 13:21:44 +0300

    added walls

---
+++ vtecha_z_labiryntu/main.py
@@ -0,0 +1,53 @@
+import pygame
+
+pygame.init()
+
+# Створення вікна гри
+screen = pygame.display.set_mode((800, 600))
+pygame.display.set_caption('Втеча з лабіринту')
+
+# Фоновий колір
+background_color = (0, 0, 0)  # Чорний колір фону
+cell_size = 40
+
+wall_img = pygame.image.load("assets/wall.png")
+wall_img = pygame.transform.scale(wall_img, (cell_size, cell_size))
+
+maze = [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1],
+    [1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 0, 1, 1, 0, 1],
+    [1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1],
+    [1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1],
+    [1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1],
+    [1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1],
+    [1, 0, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1],
+    [1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1],
+    [1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1],
+    [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+]
+height = len(maze)
+width = len(maze[0])
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # Заповнюємо екран фоном
+    screen.fill(background_color)
+
+    for y in range(height):
+        for x in range(width):
+            if maze[y][x] == 1:
+                screen.blit(wall_img, (x * cell_size, y * cell_size))
+
+    # Оновлюємо екран
+    pygame.display.flip()
+
+pygame.quit()
