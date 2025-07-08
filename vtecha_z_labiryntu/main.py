1801d520c95e3ea1d89a934fb48e2bbf05cc97b2
Vasyl Boliukh <vskesha@gmail.com>
2025-07-08 13:38:53 +0300

    added key and door on empty cells

--- vtecha_z_labiryntu/main.py
+++ vtecha_z_labiryntu/main.py
@@ -1,4 +1,6 @@
 import pygame
+import random
+
 
 pygame.init()
 
@@ -12,6 +14,12 @@ cell_size = 40
 
 wall_img = pygame.image.load("assets/wall.jpg")
 wall_img = pygame.transform.scale(wall_img, (cell_size, cell_size))
+
+key_img = pygame.image.load("assets/key.png")
+key_img = pygame.transform.scale(key_img, (cell_size, cell_size))
+
+door_img = pygame.image.load("assets/door.png")
+door_img = pygame.transform.scale(door_img, (cell_size, cell_size))
 
 maze = [
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
@@ -33,6 +41,15 @@ maze = [
 height = len(maze)
 width = len(maze[0])
 
+free_cells = []
+for y in range(height):
+    for x in range(width):
+        if maze[y][x] == 0:
+            free_cells.append((x, y))
+
+key_position = random.choice(free_cells[1:-1])
+door_position = free_cells[-1]
+
 running = True
 while running:
     for event in pygame.event.get():
@@ -46,6 +63,9 @@ while running:
         for x in range(width):
             if maze[y][x] == 1:
                 screen.blit(wall_img, (x * cell_size, y * cell_size))
+
+    screen.blit(key_img, (key_position[0] * cell_size, key_position[1] * cell_size))
+    screen.blit(door_img, (door_position[0] * cell_size, door_position[1] * cell_size))
 
     # Оновлюємо екран
     pygame.display.flip()
